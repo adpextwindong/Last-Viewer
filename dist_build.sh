443ecddc8f1a2f581e7316d4a6aaa1a4038dfb5c
Takumi Crary <tcrary@gmail.com>
2020-01-10 10:31:39 +0900

    Fix dist build issue

--- dist_build.sh
+++ dist_build.sh
@@ -7,11 +7,11 @@ rm -rf dist/
 echo "Copying project files to dist/"
 
 mkdir -p dist/data
-mkdir -p dist/js
 mkdir -p dist/css
+mkdir -p dist/lib
 cp -r data/ dist/
-cp -r js/ dist/
 cp -r css/ dist/
+cp -r lib/ dist/
 
 cp foot_viewer.html dist/index.html
 cp bundle.js dist/
