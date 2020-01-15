1dcb11541d2a94a9531750dface52d1f26e793b5
Takumi Crary <tcrary@gmail.com>
2020-01-15 12:29:29 +0900

    Migrate build script to webpack

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 echo "Sassing style.scss into style.css"
 sass css/style.scss css/style.css
 echo "Building bundle.js"
-browserify js/app.js -o bundle.js --d
+npx webpack --config webpack.config.js --devtool source-map
+
+#Browserify has been deprecated due to import/require syntax issues
+#browserify js/app.js -o bundle.js --d
