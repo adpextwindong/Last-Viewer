a65a93acc45e9fd86153993aec005e0dfc282279
Takumi Crary <tcrary@gmail.com>
2020-01-21 16:33:50 +0900

    Fix the dist script

--- dist_build.sh
+++ dist_build.sh
@@ -1,3 +1,4 @@
+rm *.bundle.js*
 echo "Running build script"
 ./build.sh
 
@@ -9,9 +10,18 @@ echo "Copying project files to dist/"
 mkdir -p dist/data
 mkdir -p dist/css
 mkdir -p dist/lib
+mkdir -p dist/deps/vue
+mkdir -p dist/deps/Three
+mkdir -p deps/vue
+mkdir -p deps/Three
+
+cp node_modules/three/build/* deps/Three
+cp node_modules/vue/dist/* deps/vue
+
+cp -r deps/ dist/deps/
 cp -r data/ dist/
 cp -r css/ dist/
 cp -r lib/ dist/
 
 cp foot_viewer.html dist/index.html
-cp bundle.js dist/
+cp *.bundle.js dist/
