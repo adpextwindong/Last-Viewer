d584f442d89268f537697583f3163b1644c5ae6b
Takumi Crary <tcrary@gmail.com>
2020-04-09 15:47:36 +0900

    Add vuex import dependency

--- dist_build.sh
+++ dist_build.sh
@@ -11,14 +11,17 @@ mkdir -p dist/data
 mkdir -p dist/css
 mkdir -p dist/lib
 mkdir -p dist/deps/vue
+mkdir -p dist/deps/vuex
 mkdir -p dist/deps/vue-router
 mkdir -p dist/deps/Three
 mkdir -p deps/vue
+mkdir -p deps/vuex
 mkdir -p deps/vue-router
 mkdir -p deps/Three
 
 cp node_modules/three/build/* deps/Three
 cp node_modules/vue/dist/* deps/vue
+cp node_modules/vuex/dist/* deps/vuex
 cp node_modules/vue-router/dist/* deps/vue-router
 cp vue.min.js deps/vue/vue.min.js
 
