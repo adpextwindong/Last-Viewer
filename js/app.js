267efe13abc48570d8184aa47c3547cfb225cee5
Takumi Crary <tcrary@gmail.com>
2020-06-15 15:44:42 +0900

    Split out the Viewer component into its own folder

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 import VueRouter from 'vue-router';
 import VueTranslate from 'vue-translate-plugin';
 import store from './store';
-Vue.use(VueRouter);
+
 Vue.use(VueTranslate);
 
 
 import Scan_Selector from './components/scan_selector';
 import Settings_Menu from './components/settings_menu';
-import Viewer from './components/viewer_layout';
+import Viewer from './components/viewer/viewer_layout';
 
 //These imports could be moved down and asyncly done from viewer probably
 Vue.component('vue-context', () => import('vue-context'));
-Vue.component('landmark_list', () => import('./components/landmark_list'));
-Vue.component('landmark_list_item', () => import('./components/landmark_list_item'));
-Vue.component('scene_graph_hiearchy', () => import('./components/scene_graph_hiearchy'));
-Vue.component('view_controls', () => import('./components/view_controls.js'));
-Vue.component('landmark_nametag', () => import('./components/landmark_nametag'));
+Vue.component('landmark_list', () => import('./components/viewer/landmark_list'));
+Vue.component('landmark_list_item', () => import('./components/viewer/landmark_list_item'));
+Vue.component('scene_graph_hiearchy', () => import('./components/viewer/scene_graph_hiearchy'));
+Vue.component('view_controls', () => import('./components/viewer/view_controls.js'));
+Vue.component('landmark_nametag', () => import('./components/viewer/landmark_nametag'));
 const CONFIG = require("./config");
 
 var app = new Vue({
