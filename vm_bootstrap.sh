f1c0378fd23fb41111461c1b6466069240738d97
Takumi Crary <tcrary@gmail.com>
2021-01-20 06:58:00 +0000

    Bump vm node version to 15.x

--- vm_bootstrap.sh
+++ vm_bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 sudo npm i sass -g
